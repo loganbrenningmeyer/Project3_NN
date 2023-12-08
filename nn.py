da64a0b626c43fb5b849cffa54a04636ba3043a0
Logan Brenningmeyer <loganbrenningmeyer@gmail.com>
2023-12-08 04:07:33 -0600

    Create nn.py

---
+++ nn.py
@@ -0,0 +1,21 @@
+# include our Python packages
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.autograd import Function
+import torch.optim as optim
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Make an MLP
+class MLP(nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(MLP, self).__init__()
+        self.linear1 = nn.Linear(D_in, H)
+        self.linear2 = nn.Linear(H, D_out)
+    def forward(self, x):
+        h_pred = F.relu(self.linear1(x))
+        y_pred = self.linear2(h_pred)
+        return y_pred
